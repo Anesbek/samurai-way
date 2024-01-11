6d2abc9e51e1f7651ebef2abedb38aa5e820d608
Anesbek <anes.amirbek12@gmail.com>
2024-01-11 18:44:39 +0600

    lesson 29

--- src/components/dialogs/Dialogs.tsx
+++ src/components/dialogs/Dialogs.tsx
@@ -19,7 +19,7 @@ const Dialogs = (props: any) => {
             </div>
             <div className={s.messeges}>
                 {messegesElemeents}
-                <p>1</p>
+
             </div>
         </div>
     );
